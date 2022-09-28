e5f414acd02fdade80284c1002d4cb80afd1e97d
JonnyHam <jonajonkim@gmail.com>
2022-09-28 18:32:58 -0500

    Initial commit

---
+++ src/com/test/Runner.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import static java.lang.System.out;
+
+public class Runner {
+    public static void main (String[] args)
+    {
+        Test test = new Test(1000);
+        out.println(test);
+    }
+}
---
+++ src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Test {
+    private final int num;
+    public Test (int n) {
+        num = n;
+    }
+
+    public String toString () {
+        return "" + num;
+    }
+}
