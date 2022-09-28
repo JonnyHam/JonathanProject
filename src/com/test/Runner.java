3b91cf752242577c1a06fc7dddf5894d1a272843
JonnyHam <jonajonkim@gmail.com>
2022-09-28 18:35:05 -0500

    hi

--- src/com/test/Runner.java
+++ src/com/test/Runner.java
@@ -5,7 +5,7 @@ import static java.lang.System.out;
 public class Runner {
     public static void main (String[] args)
     {
-        Test test = new Test(1000);
+        Test test = new Test(100);
         out.println(test);
     }
 }
